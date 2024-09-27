39875f0d1909bb8b384567e378258f2080dfb836
kels7 <hiketabo@gmail.com>
2024-09-27 08:58:23 +0000

    chore: show snackbar when response is null

--- frontend/src/js/transaction_result_handler.js
+++ frontend/src/js/transaction_result_handler.js
@@ -1,22 +1,28 @@
 export const TransactionResultHandler = (createSnack) => {
 	function handleTransaction(response, callback){
-			// Check for different types of errors
-			if(response === null){
-				console.log('Transaction responded with null')
-				return
-			}
-			if (response?.errors || response.error || !response.success) {
-				console.error('Transaction failed:', response.errors || response.error || 'Unknown error');
-				createSnack({
-					title: 'Transaction failed',
-					body: response.errors?.message || response.error?.message || 'An unexpected error occurred',
-					type: 'error'
-				})
-				return;
-			}
-			console.log('Transaction succeeded:', response);
-			if(typeof callback === 'function') callback(response);
+		console.log("txResponse: ", response);
+		// Check for different types of errors
+		if(response === null){
+			console.log('Transaction responded with null')
+			createSnack({
+				title: 'Null response',
+				body: 'Transaction responded with null',
+				type: 'error'
+			})
+			return
 		}
+		if (response?.errors || response.error || !response.success) {
+			console.error('Transaction failed:', response.errors || response.error || 'Unknown error');
+			createSnack({
+				title: 'Transaction failed',
+				body: response.errors?.message || response.error?.message || 'An unexpected error occurred',
+				type: 'error'
+			})
+			return;
+		}
+		console.log('Transaction succeeded:', response);
+		if(typeof callback === 'function') callback(response);
+	}
 	
 	function handleTransactionError(error){
 		console.error('Transaction error:', error);
