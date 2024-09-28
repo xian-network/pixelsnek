eaa3ba695838455d7ee90890fa255a229998f9bd
kels7 <hiketabo@gmail.com>
2024-09-28 10:47:00 +0000

    chore: use graphql

--- frontend/src/routes/frames/[uid].json.js
+++ frontend/src/routes/frames/[uid].json.js
@@ -1,14 +1,25 @@
+import { getThingByUid } from "../../js/graphqlQueries";
+import { extractThingValues, makeGraphQLRequest } from "../../js/processGraphql";
+
 export async function get(req, res, next) {
 	const { uid } = req.params;
 
 	let auctionInfo = null
-	let thingInfo = await global.db.models.PixelFrame.findOne({uid})
 
-	if (!thingInfo) thingInfo = null
-	else {
-		if (thingInfo.blacklist) thingInfo = null
-		auctionInfo = await global.db.queries.getActiveAuction(uid)
-	}
+	const thingQuery = getThingByUid(uid);
+	const thingQueryResults = await makeGraphQLRequest(thingQuery);
+	const thingData = thingQueryResults?.data
+	const thingInfo = extractThingValues(uid, thingData);
+
+	//let thingInfo = await global.db.models.PixelFrame.findOne({uid})
+
+	// if (!thingInfo) thingInfo = null
+	// else {
+	// 	if (thingInfo.blacklist) thingInfo = null
+	// 	auctionInfo = await global.db.queries.getActiveAuction(uid)
+	// }
+
+	
 
 	res.setHeader('Content-Type', 'application/json');
 	res.end(JSON.stringify({thingInfo, auctionInfo}));
