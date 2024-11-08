dfa695fdb35b50e6a0d83f528bf138e101e20d21
kels7 <hiketabo@gmail.com>
2024-11-08 04:16:40 +0000

    chore: use adapter node

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// kit config options go here
+		adapter: adapter()
 	},
 	vite: {
 		ssr: {
