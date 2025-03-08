6766dc6a385bbf0c43947f82cbf95bd37d46a66b
kels7 <hiketabo@gmail.com>
2025-03-08 13:47:38 +0000

    fix: make bignumber

--- frontend/src/components/FormBid.svelte
+++ frontend/src/components/FormBid.svelte
@@ -22,7 +22,7 @@
 	
 	let reservePrice = toBigNumber($showModal.modalData.auctionInfo.reserve_price || 1)
 	
-	$: bidAmount = $showModal.modalData.winning_bid ? $showModal.modalData.winning_bid.plus(1) : reservePrice
+	$: bidAmount = $showModal.modalData.winning_bid ? toBigNumber($showModal.modalData.winning_bid).plus(1) : reservePrice
 	$: below_reserve = $showModal.modalData.winning_bid ? false : bidAmount.isLessThan(reservePrice)
 	$: approvalTxStamps_to_tau = bidAmount.isGreaterThan(toBigNumber($approvalAmount[config.masterContract])) ? toBigNumber(stampLimits.currency.approve).dividedBy($stampRatio) : toBigNumber(0)
 	$: bidTxStamps_to_tau = toBigNumber(stampLimits[config.auctionContract].bid).dividedBy($stampRatio)
