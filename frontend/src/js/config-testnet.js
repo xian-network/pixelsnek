d3cb8924d2454f87e721a23cc713a58163e30f2f
duelingbenjos <getrektwithme1@gmail.com>
2025-05-08 10:04:49 +0100

    testnet config

---
+++ frontend/src/js/config-testnet.js
@@ -0,0 +1,43 @@
+export const config = {
+    frameWidth: 25,
+    totalPixels: 625,
+    totalFrames: 8,
+    infoContract: "con_pixel_frames_info_v0_3", // CHANGE
+    masterContract: "con_pixel_frames_master_v0_3", // CHANGE
+    auctionContract: "con_pixel_frames_auction_v0_1", // CHANGE
+    networkType: "testnet", // CHANGE
+    currencySymbol: "XIAN", // CHANGE
+    domainName: "https://pixelsnek.xian.org", // CHANGE
+    //blockExplorer: "http://localhost:1337", // CHANGE
+    blockExplorer: "https://testnet-explorer.xian.org", // CHANGE
+    masternode: "https://testnet.xian.org", // CHANGE
+    graphqlEndpoint: "https://testnet.xian.org/graphql" // CHANGE
+}
+
+export const featureLocks = {
+    auctions: {
+        locked: false,
+        whitelist: []
+    }
+}
+
+export const socket_config = {
+    host: "http:localhost", // DECIDE
+    port: 3001 // CHANGE
+}
+
+
+export const stampLimits = (()=>{
+    let stampValues = {}
+    stampValues[config.masterContract] = {
+        buy_thing: 90, // CHANGE
+        sell_thing: 90, // CHANGE
+        like_thing: 90, // CHANGE
+        transfer: 105, // CHANGE
+        prove_ownership: 95 // CHANGE
+    }
+    stampValues['currency'] = {
+        approve: 25
+    }
+    return stampValues
+})()
